c72fffedf4564bf2e60fc60ed1cf667161a0feef
Anil Puttabuddhi <anil.smp@gmail.com>
2020-07-13 17:54:00 +0100

    Initial Commit

---
+++ DemoHueRemote/DemoHueRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoHueRemoteApp.swift
+//  DemoHueRemote
+//
+//  Created by Anil Puttabuddhi on 13/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct DemoHueRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
